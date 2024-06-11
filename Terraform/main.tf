2b096364a35ec7f2afa8934a4f1b47f22ed3d199
jbermejo14 <jpbermejobramley@gmail.com>
2024-06-11 11:02:05 +0200

    Added Terrafrom and restructured the app

---
+++ Terraform/main.tf
@@ -0,0 +1,15 @@
+resource "aws_security_group" "ASC_SG" {
+  name = "ASG_SG"
+  description = "Security Group inbounds traffic to Docker container and SSH"
+  vpc_id = "vpc-017f8b31653169d1f"
+}
+
+resource "aws_instance" "ASC_Instance" {
+  ami           = "ami-0d191299f2822b1fa"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-01df6f4eb4ec8187d"
+
+  tags = {
+    Name = "ASC_Instance"
+  }
+}
